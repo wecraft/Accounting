daeea31053b4f2bfcba7fe59600062b093e6f735
wecraft <valentin@wecraftmedia.com>
2020-02-03 00:16:23 +0200

    fix warnings

--- api/app/UserPie.php
+++ api/app/UserPie.php
@@ -18,7 +18,7 @@ class UserPie extends Model
 
     public function getAmountResourcable()
     {
-        return round($this->amount * 100);
+        return round($this->amount * 100, 1);
     }
 
     public function user()
