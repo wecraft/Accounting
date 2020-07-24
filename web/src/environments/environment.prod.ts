f89f74ac4704369e2785664863cb357c7ae77991
wecraft <valentin@wecraftmedia.com>
2020-07-24 20:01:19 +0300

    change api address

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	apiHost: "https://api.accounting.wecraftmedia.com/api",
+	apiHost: "https://api-accounting.wecraftmedia.com/api",
 
 	origin: "https://accounting.wecraftmedia.com"
 };
